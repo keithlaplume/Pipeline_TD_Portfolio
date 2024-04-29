78ceebe9c0fc9a2214bfecf2da70da3193bb6784
Keith LaPlume <contact@keithlaplume.com>
2024-04-29 19:50:56 -0400

    add description

--- fusion_scripts/set_up_camera.py
+++ fusion_scripts/set_up_camera.py
@@ -1,6 +1,7 @@
 """This code is considered proprietary property of Snowball Studios and its affiliates and is not for re-use in any way.
 It is being provided to as sample code and portions have been blocked out to make it deliberately unusable.
 If you are interested in reviewing a more complete code example with me please contact me at contact@keithlaplume.com"""
+"""This script sets up the selected camera with the correct lens shift"""
 render_cam = comp.ActiveTool
 
 if render_cam:
